d45ce8a396b4569376d6ce0a6cdaa37d8a192a80
Matheus Santos <matheusps0012@gmail.com>
2021-11-08 16:43:28 -0300

    Add - OAuth2 Authencation with GCP

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View } from "react-native";
+import * as AuthSession from "expo-auth-session";
+import { useNavigation } from "@react-navigation/native";
 
-import { Button } from '../../components/Button';
-import { SignInContent } from '../../components/SignInContent';
+import { Button } from "../../components/Button";
+import { SignInContent } from "../../components/SignInContent";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 type AuthResponse = {
   type: string;
   params: {
     access_token: string;
-  }
-}
+  };
+};
 
 export function SignIn() {
   const navigation = useNavigation();
 
+  //obter informações do usuário
   async function handleSignIn() {
-    navigation.navigate('Profile');
+    const RESPONSE_TYPE = "token";
+    const ESCOPE = encodeURI("profile email");
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${ESCOPE}`;
+    const { type, params } = (await AuthSession.startAsync({
+      authUrl,
+    })) as AuthResponse;
+    if (type === "success") {
+      navigation.navigate("Profile", { token: params.access_token });
+    }
   }
 
   return (
